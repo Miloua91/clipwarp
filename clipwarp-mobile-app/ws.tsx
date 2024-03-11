792f78ef168e9f2f28b3adb272bac7d1b1bacc7b
Miloua Mokhtar <miloua23@gmail.com>
2024-03-11 16:42:41 +0000

    added unique id to websocket url

--- clipwarp-mobile-app/ws.tsx
+++ clipwarp-mobile-app/ws.tsx
@@ -1,56 +1,64 @@
 import { useState, useEffect } from "react";
 import { View, TextInput, Text } from "react-native";
 import AwesomeButton from "react-native-really-awesome-button";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { FontAwesome } from '@expo/vector-icons';
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { FontAwesome } from "@expo/vector-icons";
 
 export function WS() {
   const [wsAddress, setWsAddress] = useState(`192.168.1.`);
 
-// Load the WebSocket address from AsyncStorage on component mount
+  // Load the WebSocket address from AsyncStorage on component mount
   useEffect(() => {
     const loadWsAddress = async () => {
       try {
-        const savedAddress = await AsyncStorage.getItem('address');
+        const savedAddress = await AsyncStorage.getItem("address");
         if (savedAddress !== null) {
           setWsAddress(savedAddress);
-	  console.log(savedAddress);
+          console.log(savedAddress);
         }
       } catch (error) {
-        console.error('Error loading WebSocket address:', error);
+        console.error("Error loading WebSocket address:", error);
       }
     };
 
     loadWsAddress();
   }, []);
 
-// Function to handle changing the WebSocket address
+  // Function to handle changing the WebSocket address
   const handleWsAddressChange = async () => {
-
     // Store the updated WebSocket address in AsyncStorage
-     if (wsAddress !== null) {
-    try {
-      await AsyncStorage.setItem('address', wsAddress);
-    } catch (error) {
-      console.error('Error saving WebSocket address:', error);
+    if (wsAddress !== null) {
+      try {
+        await AsyncStorage.setItem("address", wsAddress);
+      } catch (error) {
+        console.error("Error saving WebSocket address:", error);
+      }
     }
-  }  };
+  };
 
-	return (
-	<>
-	<Text className="text-lg font-bold">Enter server's IP</Text>
-	<View className="w-full flex flex-row space-x-1">
-        <TextInput className="w-fit border-2 rounded pl-4 text-lg font-semibold" placeholder="Enter WebSocket address" onChangeText={text => setWsAddress(text)} value={wsAddress} />
-	<AwesomeButton backgroundColor="white" width={60} onPress={handleWsAddressChange}>
-	<FontAwesome name="save" size={24} color="black" />
-	</AwesomeButton>
-	</View>
-	</>
-	)
+  return (
+    <>
+      <Text className="text-lg font-bold">Enter server's IP</Text>
+      <View className="w-full flex flex-row space-x-1">
+        <TextInput
+          className="w-fit border-2 rounded pl-4 text-lg font-semibold"
+          placeholder="Enter WebSocket address"
+          onChangeText={(text) => setWsAddress(text)}
+          value={wsAddress}
+        />
+        <AwesomeButton
+          backgroundColor="white"
+          width={60}
+          onPress={handleWsAddressChange}
+        >
+          <FontAwesome name="save" size={24} color="black" />
+        </AwesomeButton>
+      </View>
+    </>
+  );
 }
 
 export const webSocket = async () => {
-  const wsAddress = await AsyncStorage.getItem('address');
-  return new WebSocket(`ws://${wsAddress}:5678/`);
+  const wsAddress = await AsyncStorage.getItem("address");
+  return new WebSocket(`ws://${wsAddress}:5678/69`);
 };

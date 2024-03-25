48b6f3e3f9174a9481164e235b2f381bcdc833e9
Miloua Mokhtar <miloua23@gmail.com>
2024-03-25 23:43:56 +0100

    added socketio

--- mobile-app/assets/servedb.py
+++ mobile-app/assets/servedb.py
@@ -2,8 +2,10 @@ import sqlite3
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 CORS(app)
 
@@ -30,6 +32,8 @@ def get_clips():
         for clip in clips
     ]
 
+    socketio.emit("refresh")
+
     return jsonify(clip_data)
 
 
@@ -44,9 +48,29 @@ def delete_clip(clip_id):
 
     conn.close()
 
+    socketio.emit("refresh")
+
     # Return a success message or status code
     return jsonify({"message": "Clip deleted successfully"}), 200
 
 
+@app.route("/reset", methods=["POST"])
+def reset_database():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    # Execute SQL commands to delete all records from each table
+    cursor.execute("DELETE FROM clips")
+    # Add more DELETE statements if you have other tables
+
+    conn.commit()
+    conn.close()
+
+    socketio.emit("refresh")
+
+    # Return a success message
+    return jsonify({"message": "Database reset successfully"}), 200
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, host="0.0.0.0", debug=True)

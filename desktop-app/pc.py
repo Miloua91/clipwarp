2d30039889b2db00ea65085b763ebd00297b34a8
Miloua Mokhtar <miloua23@gmail.com>
2024-03-27 01:25:32 +0100

    add clips to db

--- desktop-app/pc.py
+++ desktop-app/pc.py
@@ -1,20 +1,30 @@
-from PyQt5.QtCore import QObject, pyqtSignal
 import asyncio
+import socket
+
 import websockets
+from PyQt5.QtCore import QObject, pyqtSignal
+
 
 class Client(QObject):
     recv_signal = pyqtSignal(str)
+
     def run(self):
         asyncio.run(self.client())
 
+    def get_ip_address(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+        s.close()
+        return ip
 
     async def client(self):
-        uri = "ws://localhost:5678"
+        uri = f"ws://{self.get_ip_address()}:5678/Server"
         async with websockets.connect(uri) as self.websocket:
             async for message in self.websocket:
                 self.recv_signal.emit(message)
 
-    def bridge(self,msg):
+    def bridge(self, msg):
         asyncio.run(self.send_message(msg))
 
     async def send_message(self, message):

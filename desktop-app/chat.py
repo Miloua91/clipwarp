8c908f130c5d71a32c0d6557f3c1f6652dca8c61
Miloua Mokhtar <miloua23@gmail.com>
2024-04-04 16:43:23 +0100

    updated chat ui

--- desktop-app/chat.py
+++ desktop-app/chat.py
@@ -1,36 +1,98 @@
-from PyQt5.QtWidgets import  QWidget
-from PyQt5.QtWidgets import QApplication
+import requests
+import socketio
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtGui import QIcon
-import socket
-from widget_ui import Ui_Widget
+from PyQt5.QtWidgets import *
 
-class Chat(QWidget):
+from ui import Ui_MainWindow
+
+
+class Chat(QMainWindow):
     message_signal = pyqtSignal(str)
+
     def __init__(self):
         super().__init__()
-        self.ui = Ui_Widget()
+        self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.setStyleSheet(
+            """
+            QPlainTextEdit, QMainWindow, QLabel, QPushButton, VerticalTabWidget {
+                background-color: #333;
+                color: #FFF;
+            }
+            QPlainTextEdit,TabBar {
+                background-color: #4d4d4d;
+                color: #FFF;
+            }
+            TabBar::tab {
+                width: 32px;
+                height: 140px; 
+            }
+        """
+        )
+
+        self.socketio = socketio.Client()
+
+        def on_refresh():
+            print("wssup")
+
+        self.socketio.on("refresh", on_refresh)
+        self.socketio.connect("http://localhost:5000")
+
+        self.fetch_clips()
+
+        self.setFixedSize(612, 392)
+
         icon = QIcon("./assets/clipwarp.png")
         self.setWindowIcon(icon)
-        self.ui.ip_label.setText(f'ip address: {self.get_ip_address()}')
-        self.ui.sendButton.clicked.connect(self.on_button_click)
-        self.ui.copyButton.clicked.connect(self.copy_text)
-        self.ui.textEdit_2.setReadOnly(True)
+        self.tray = QSystemTrayIcon(self)
+
+        # Check if System supports STray icons
+        if self.tray.isSystemTrayAvailable():
+            self.tray.setIcon(self.windowIcon())
+
+            # Context Menu
+            ctmenu = QMenu()
+            actionshow = ctmenu.addAction("Show/Hide")
+            actionshow.triggered.connect(
+                lambda: self.hide() if self.isVisible() else self.show()
+            )
+            actionquit = ctmenu.addAction("Quit")
+            actionquit.triggered.connect(self.close)
+
+            self.tray.setContextMenu(ctmenu)
+            self.tray.show()
+        else:
+            # Destroy unused var
+            self.tray = None
+
+        # Show App
+        self.show()
+        self.ui.Send.clicked.connect(self.on_button_click)
+        self.ui.Paste.clicked.connect(self.paste_text)
+
+    def fetch_clips(self):
+        response = requests.get("http://localhost:5000")
+        if response.status_code == 200:
+            clips = response.json()
+            self.ui.load_clips(clips)
+        else:
+            print("Failed to fetch clips from server")
 
     def show_msg(self, msg):
-        self.ui.textEdit_2.setText(msg)
+        self.fetch_clips()
 
     def on_button_click(self):
-        message = self.ui.textEdit.toPlainText()
-        if message != '':
+        message = self.ui.plainTextEdit.toPlainText()
+        if message != "":
             self.message_signal.emit(message)
-            self.ui.textEdit.setText('')
+            self.ui.plainTextEdit.setPlainText("")
+            self.fetch_clips()
 
-    def copy_text(self):
-        text = self.ui.textEdit_2.toPlainText()
+    def paste_text(self):
+        text_edit = self.ui.plainTextEdit
         clipboard = QApplication.clipboard()
-        clipboard.setText(text)
+        text_edit.insertPlainText(clipboard.text())
 
     def get_ip_address(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)

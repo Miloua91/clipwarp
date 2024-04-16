932ce642586bdf484a575a9cb6e6c94c7e8f1ca2
Miloua Mokhtar <miloua23@gmail.com>
2024-04-16 23:26:18 +0100

    minor changes

--- mobile-app/ws.tsx
+++ mobile-app/ws.tsx
@@ -1,17 +1,19 @@
 import { useState, useEffect } from "react";
-import { View, TextInput, Text, Pressable } from "react-native";
+import { View, TextInput, Text, Pressable, StyleSheet } from "react-native";
 import AwesomeButton from "react-native-really-awesome-button";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { FontAwesome } from "@expo/vector-icons";
-import * as Device from 'expo-device';
-import * as Updates from 'expo-updates';
+import * as Device from "expo-device";
+import * as Updates from "expo-updates";
+
+const bgColor = "#252422";
+const cardColor = "#403d39";
 
 export function WS() {
   const [wsAddress, setWsAddress] = useState(`192.168.1.`);
-  let getDevice = `${Device.deviceName}`
-  let device = getDevice.replace(/\s+/g, '-');
+  let getDevice = `${Device.deviceName}`;
+  let device = getDevice.replace(/\s+/g, "-");
   const [deviceName, setDeviceName] = useState(device);
-
 
   // Load the WebSocket address from AsyncStorage on component mount
   useEffect(() => {
@@ -37,7 +39,6 @@ export function WS() {
       }
     };
 
-
     loadWsAddress();
     loadDevice();
   }, []);
@@ -55,10 +56,10 @@ export function WS() {
   };
 
   const saveDeviceName = async () => {
-    if (deviceName !== null && deviceName !== '') {
+    if (deviceName !== null && deviceName !== "") {
       try {
         await AsyncStorage.setItem("device", deviceName);
-        await new Promise(resolve => setTimeout(resolve, 80));
+        await new Promise((resolve) => setTimeout(resolve, 80));
         await Updates.reloadAsync();
       } catch (error) {
         console.error("Error saving device name:", error);
@@ -66,54 +67,58 @@ export function WS() {
     }
   };
 
-
   return (
     <View className="w-full m-auto mt-6">
-      <View className="space-x-1 mb-4 mx-6 border rounded-xl">
-        <Text className="mx-2 text-lg font-semibold py-1">Enter server's IP</Text>
+      <View
+        className="space-x-1 mb-4 mx-6 border rounded-xl"
+        style={styles.card}
+      >
+        <Text className="mx-2 text-lg font-semibold py-1 text-white">
+          Enter server's IP
+        </Text>
         <TextInput
-          className="w-[75%] pl-4 text-lg m-1 bottom-2"
+          className="w-[75%] pl-4 text-lg m-1 bottom-2 text-white"
           placeholder="IP address"
           onChangeText={(text) => setWsAddress(text)}
           value={wsAddress}
         />
-        <View 
-          className="absolute right-0 mx-2 my-1"
-        >
-        <AwesomeButton
-          backgroundColor="white"
-          width={60}
-          onPress={handleWsAddressChange}
-        >
-          <FontAwesome name="save" size={24} color="black" />
-        </AwesomeButton>
+        <View className="absolute right-0 mx-2 my-1">
+          <AwesomeButton
+            backgroundColor="#403d39"
+            width={60}
+            onPress={handleWsAddressChange}
+          >
+            <FontAwesome name="save" size={24} color="white" />
+          </AwesomeButton>
         </View>
       </View>
 
-      <View className="space-x-1 mx-6 mb-4 border rounded-xl">
-        <Text className="mx-2 text-lg font-semibold py-1">Enter device's name</Text>
+      <View
+        className="space-x-1 mx-6 mb-4 border rounded-xl"
+        style={styles.card}
+      >
+        <Text className="mx-2 text-lg font-semibold py-1 text-white">
+          Enter device's name
+        </Text>
         <TextInput
-          className="w-[75%] pl-4 text-lg m-1 bottom-2"
+          className="w-[75%] pl-4 text-lg m-1 bottom-2 text-white"
           placeholder={getDevice}
           onChangeText={(text) => setDeviceName(text)}
-          value={deviceName.replace(/-/g, ' ')}
+          value={deviceName.replace(/-/g, " ")}
         />
         {!deviceName.trim() && (
           <Text className="text-red-500 p-1">Please enter a device name</Text>
         )}
-        <View 
-          className="absolute right-0 mx-2 my-1"
-        >
-        <AwesomeButton
-          backgroundColor="white"
-          width={60}
-          onPress={saveDeviceName}
-        >
-          <FontAwesome name="save" size={24} color="black" />
-        </AwesomeButton>
+        <View className="absolute right-0 mx-2 my-1">
+          <AwesomeButton
+            backgroundColor="#403d39"
+            width={60}
+            onPress={saveDeviceName}
+          >
+            <FontAwesome name="save" size={24} color="white" />
+          </AwesomeButton>
         </View>
       </View>
-
     </View>
   );
 }
@@ -121,6 +126,15 @@ export function WS() {
 export const webSocket = async () => {
   const wsAddress = await AsyncStorage.getItem("address");
   const getDevice = await AsyncStorage.getItem("device");
-  const device = getDevice?.replace(/\s+/g, '-');
+  const device = getDevice?.replace(/\s+/g, "-");
   return new WebSocket(`ws://${wsAddress}:5678/${device}`);
 };
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: `${cardColor}`,
+  },
+  background: {
+    backgroundColor: `${bgColor}`,
+  },
+});

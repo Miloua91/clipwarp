c1b03a2f703f4aafedc9862fbf0b073979f9e9ff
Miloua Mokhtar <miloua23@gmail.com>
2024-04-19 22:34:30 +0100

    formated code

--- desktop-app/tray.py
+++ desktop-app/tray.py
@@ -1,9 +1,10 @@
-
 import os
-from pystray import Icon, Menu as menu, MenuItem as item
-from PyQt5.QtCore import QObject, pyqtSignal
-from PIL import Image, ImageDraw
 
+from PIL import Image, ImageDraw
+from PyQt5.QtCore import QObject, pyqtSignal
+from pystray import Icon
+from pystray import Menu as menu
+from pystray import MenuItem as item
 
 
 class Tray(QObject):
@@ -12,15 +13,17 @@ class Tray(QObject):
 
     def __init__(self):
         super().__init__()
-        self.icon = Icon(
-            'CLipwarp',
-        icon=Image.open("./assets/clipwarp.png"))
-        self.icon.menu = menu(item(text="Open",action=lambda: self.icon_clicked.emit(), default=True), item('Exit', self.exit_icon))        
+        self.icon = Icon("CLipwarp", icon=Image.open("./assets/clipwarp.png"))
+        self.icon.menu = menu(
+            item(text="Open", action=lambda: self.icon_clicked.emit(), default=True),
+            item("Exit", self.exit_icon),
+        )
+
     def run(self):
         self.icon.run()
 
-
     def exit_icon(self):
-        self.icon.visible=False
+        self.icon.visible = False
         self.icon.stop()
         self.exit_signal.emit()
+

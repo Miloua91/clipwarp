610317ae4ce625e5d5c25a739d93066dea3b7b63
Miloua Mokhtar <miloua23@gmail.com>
2024-05-15 15:01:22 +0100

    fixed metroconfig

--- mobile-app/metro.config.js
+++ mobile-app/metro.config.js
@@ -15,13 +15,14 @@ const {
 
 const config = {
   transformer: {
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+    assetPlugins: ["expo-asset/tools/hashAssetFiles"],
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: true,
+        inlineRequires: false,
       },
     }),
-    babelTransformerPath: require.resolve("react-native-svg-transformer"),
   },
   resolver: {
     assetExts: assetExts.filter((ext) => ext !== "svg"),

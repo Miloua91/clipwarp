dbff2555d7da4c3644451d8f079deb3c9f04166d
Miloua Mokhtar <miloua23@gmail.com>
2024-05-15 15:50:17 +0100

    fixed icons not showing

--- mobile-app/babel.config.js
+++ mobile-app/babel.config.js
@@ -1,7 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel","react-native-reanimated/plugin",],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "nativewind/babel",
+      "react-native-reanimated/plugin",
+      "transform-inline-environment-variables",
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@": "./src",
+            "react-native-vector-icons": "@expo/vector-icons",
+          },
+        },
+      ],
+    ],
+    env: {
+      production: {
+        plugins: ["react-native-paper/babel"],
+      },
+    },
   };
 };

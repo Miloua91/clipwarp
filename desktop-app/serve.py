3eeba21185c9bed0346723fd99d2997ca70a8d47
Miloua Mokhtar <miloua23@gmail.com>
2024-05-26 17:48:10 +0100

    serve html over ssl

--- desktop-app/serve.py
+++ desktop-app/serve.py
@@ -1,4 +1,5 @@
 import socket
+import subprocess
 
 import uvicorn
 from blacksheep import Application
@@ -9,7 +10,9 @@ class Serve(QObject):
     def start(self):
         app = Application()
         app.serve_files("assets/dist", index_document="index.html")
-        uvicorn.run(app, host=self.get_ip_address(), port=6969)
+        uvicorn.run(app, host=self.get_ip_address(), port=6969,
+        ssl_keyfile='./assets/ip.key',
+        ssl_certfile='./assets/ip.crt')
 
     def get_ip_address(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)

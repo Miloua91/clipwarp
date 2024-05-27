fe3435333b249e7a6894811d3517a57eaa8bd6ac
Miloua Mokhtar <miloua23@gmail.com>
2024-05-27 22:25:24 +0100

    serve files over http

--- desktop-app/serve.py
+++ desktop-app/serve.py
@@ -1,5 +1,4 @@
 import socket
-import subprocess
 
 import uvicorn
 from blacksheep import Application
@@ -10,9 +9,7 @@ class Serve(QObject):
     def start(self):
         app = Application()
         app.serve_files("assets/dist", index_document="index.html")
-        uvicorn.run(app, host=self.get_ip_address(), port=6969,
-        ssl_keyfile='./assets/key.pem',
-        ssl_certfile='./assets/cert.pem')
+        uvicorn.run(app, host=self.get_ip_address(), port=6969)
 
     def get_ip_address(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)

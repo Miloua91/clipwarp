ff0090bc56d77822d417a7f77b206b8d095fd922
Miloua Mokhtar <miloua23@gmail.com>
2024-07-05 18:43:22 +0100

    fix: gradle build error because of metro misconfig

--- mobile-app/metro.config.js
+++ mobile-app/metro.config.js
@@ -1,33 +1,5 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const { getDefaultConfig } = require("expo/metro-config");
 
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const config = getDefaultConfig(__dirname);
 
-const defaultConfig = getDefaultConfig(__dirname);
-
-const {
-  resolver: { sourceExts, assetExts },
-} = getDefaultConfig(__dirname);
-
-const config = {
-  transformer: {
-    babelTransformerPath: require.resolve("react-native-svg-transformer"),
-    assetPlugins: ["expo-asset/tools/hashAssetFiles"],
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-  resolver: {
-    assetExts: assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...sourceExts, "svg"],
-  },
-};
-
-module.exports = mergeConfig(defaultConfig, config);
+module.exports = config;

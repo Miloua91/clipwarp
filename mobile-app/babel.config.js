062c42ddf9f5343e724d36fb9e5395705f0deafd
Miloua Mokhtar <miloua23@gmail.com>
2024-11-01 14:51:25 +0100

    updated native wind config

--- mobile-app/babel.config.js
+++ mobile-app/babel.config.js
@@ -1,9 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-    plugins: [
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
+    ],
+    plugins: [
       "react-native-reanimated/plugin",
       "transform-inline-environment-variables",
       [

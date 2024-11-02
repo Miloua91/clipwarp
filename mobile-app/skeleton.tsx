46d2bd0bb95bcab662eda088f7066c54b611bbaa
Miloua Mokhtar <miloua23@gmail.com>
2024-11-02 13:26:38 +0100

    updated skelton ui

--- mobile-app/skeleton.tsx
+++ mobile-app/skeleton.tsx
@@ -1,11 +1,11 @@
 import React, { useCallback, useEffect, useRef } from "react";
+import "./global.css";
 import {
   Animated,
   StyleProp,
   ViewStyle,
   View,
   Pressable,
-  StyleSheet,
   DimensionValue,
 } from "react-native";
 import { getLocales } from "expo-localization";
@@ -15,11 +15,9 @@ import {
   Feather,
   MaterialIcons,
 } from "@expo/vector-icons";
-import { ThemeProvider, useTheme } from "./ThemeContext";
+import { useTheme } from "./ThemeContext";
 
 const deviceLanguage = getLocales()?.[0]?.languageCode;
-const bgColor = "#252422";
-const cardColor = "#403d39";
 
 type SkeletonProps = {
   width: DimensionValue;
@@ -80,34 +78,34 @@ export function Skeleton() {
       className={`mb-2 w-[97%] py-2 rounded-xl bg-stone-800`}
       style={{ backgroundColor: cardBgColor }}
     >
-      <View className="px-2 w-full border-b border-stone-600 my-1 pb-4 flex flex-col">
-        <SkeletonLoader width="100%" height={28} />
+      <View className="px-2 w-full border-b border-stone-600 my-1 pb-2 flex flex-col">
+        <SkeletonLoader width="100%" height={21.5} />
         <View className="flex items-end mt-2">
-          <SkeletonLoader width={200} height={19} />
+          <SkeletonLoader width={200} height={19.3} />
         </View>
       </View>
       <View className="flex flex-row justify-between py-2 px-3">
         <Pressable
-          className={`${theme === "light" ? "active:bg-stone-300" : "active:bg-stone-600"} w-[2rem] h-9 px-[5px] py-[5px] rounded`}
+          className={`${theme === "light" ? "active:bg-stone-300" : "active:bg-stone-600"} h-10 w-10 justify-center items-center rounded`}
         >
           <Feather name="copy" size={26} color={textColor} />
         </Pressable>
         <Pressable
-          className={`${theme === "light" ? "active:bg-stone-300" : "active:bg-stone-600"} w-15 h-9 p-1 rounded`}
+          className={`${theme === "light" ? "active:bg-stone-300" : "active:bg-stone-600"} h-10 w-10 justify-center items-center rounded`}
         >
           <Entypo name="share" size={26} color={textColor} />
         </Pressable>
         <Pressable
-          className={`${theme === "light" ? "active:bg-stone-300" : "active:bg-stone-600"} w-15 h-9 px-1 py-[2px] rounded`}
+          className={`${theme === "light" ? "active:bg-stone-300" : "active:bg-stone-600"} h-10 w-10 justify-center items-center rounded`}
         >
           <MaterialIcons name="open-in-browser" size={32} color={textColor} />
         </Pressable>
         <Pressable
-          className={`${theme === "light" ? "active:bg-red-400" : "active:bg-red-500"} w-15 h-9 p-1 rounded ${
+          className={`${theme === "light" ? "active:bg-red-400" : "active:bg-red-500"} h-10 w-12 justify-center items-center rounded ${
             deviceLanguage === "ar" ? "rotate-180" : ""
           }`}
         >
-          <FontAwesome6 name="delete-left" size={28} color={textColor} />
+          <FontAwesome6 name="delete-left" size={26} color={textColor} />
         </Pressable>
       </View>
     </View>
